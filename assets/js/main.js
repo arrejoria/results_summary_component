24d756257c43e338fe196da9ae6732ff57a684ee
Lucas Exequiel <arrejoria1994@gmail.com>
2023-06-19 12:27:12 -0300

    Project Update [HTML, Styles, Scripts]

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,41 +15,63 @@ fetch("data.json")
     throw new Error("Error al obtener el archivo JSON.");
   })
   .then((data) => {
-    // Aquí puedes utilizar los datos JSON
     const sumObj = data;
+    setDataColor(sumObj);
+
     for (let key in sumObj) {
       const sumItems = sumObj[key];
-
       createCatItem(sumItems);
     }
   })
   .catch((error) => {
-    // Manejo de errores
     console.error(error);
   });
 
 function createCatItem(sumItem) {
-  const catColors = {
-    lightred: "hsl(0, 100%, 67%)",
-    orangeyyellow: "hsl(39, 100%, 56%)",
-    greenteal: "hsl(166, 100%, 37%)",
-    cobaltblue: "hsl(234, 85%, 45%)",
-  };
-  const {lightred, orangeyyellow, greenteal, cobaltblue} = colors
   const { category, score, icon, color } = sumItem;
-  setCatColor(sumItem);
-  console.log(sumItem);
   const sumLi = document.createElement("li");
   sumLi.innerHTML = `
-      <div class="summary__item" data-list-value="" data-item-color="${color}">
+      <div class="summary__item" data-list-value=""data-item-bg="${color[1][1]}" data-item-color="${color[1][0]}">
         <figure class="summary__figure">
           <img src="${icon}" alt="Reaction Image" />${category}
         </figure>
-        <p><span>${score}</span>/100</p>
+        <p><span>${score}</span> / 100</p>
       </div>
     `;
 
   summaryList.append(sumLi);
+  setColor(color[1][0], color[1][1]);
 }
 
-function setCatColor(category) {}
+function setDataColor(category) {
+  let colorOpacity;
+  const dataColors = {
+    lightred: ["hsl(0, 100%, 67%", "hsl(0, 100%, 67%, 5%)"],
+    orangeyyellow: ["hsl(39, 100%, 56%)", "hsl(39, 100%, 56%, 5%)"],
+    greenteal: ["hsl(166, 100%, 37%)", "hsl(166, 100%, 37%, 5%)"],
+    cobaltblue: ["hsl(234, 85%, 45%)", "hsl(234, 85%, 45%, 5%)"],
+  };
+  let index = 0;
+  for (let key in category) {
+    if (!category[key].hasOwnProperty("color")) {
+      const colors = Object.entries(dataColors);
+      category[key]["color"] = colors[index];
+    }
+
+    index++;
+  }
+}
+
+function setColor(color, bgColor) {
+  const summaryItem = document.querySelectorAll(".summary__item");
+  summaryItem.forEach((el) => {
+    const dataColor = el.getAttribute("data-item-color"),
+      dataBg = el.getAttribute("data-item-bg");
+    console.log(dataColor);
+    if (color == dataColor || bgColor == dataBg) {
+      el.style.borderColor = bgColor;
+      el.style.backgroundColor = bgColor;
+      el.children[0].style.color = color;
+    }
+  });
+}
